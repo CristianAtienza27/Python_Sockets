d62e3b3c390745408b9a67915064552dc112bf69
CristianAtienza27 <c.atienza@cesjuanpablosegundocadiz.es>
2021-12-02 01:17:28 +0100

    Terminado

--- Servidor.py
+++ Servidor.py
@@ -18,11 +18,13 @@ class Trivial(Thread):
         self.preguntas = preguntas      
 
     def run(self):
-        global turno, jugadores, numJugadores, tiempo, jugadoresOrdenados, cad, listaPreguntas
+        global turno, jugadores, numJugadores, tiempo, jugadoresOrdenados, cadena, listaPreguntas
 
+        #El servidor recibe la opción elegida, email y contraseña del cliente
         while self.logueado == False:
             datos = self.socket.recv(1024).decode().split(';')
 
+            #login
             if datos[0] == 'login':
                 if(login(datos[1], datos[2])):
                     self.socket.send("True".encode())
@@ -30,6 +32,7 @@ class Trivial(Thread):
                     self.logueado = True
                 else:
                     self.socket.send("False".encode())
+            #registro
             elif datos[0] == 'registro':
                 if not registro(datos[1], datos[2]):
                     self.socket.send("True".encode())
@@ -38,51 +41,64 @@ class Trivial(Thread):
             else:
                 self.socket.send("False".encode())
         
+        # recibimos nickname del cliente
         self.nombre = self.socket.recv(1024).decode() 
 
+        #ocupamos un semáforo 
         turno.acquire() 
 
         numJugadores = numJugadores + 1
 
         tiempo = tiempo + 1
 
+        #mientras los jugadores sean distintos de 4 no empieza la partida
         while numJugadores != 2:
             pass
 
         print('¡Comienza la partida!')
         sleep(2)
 
+        #envía las 5 preguntas de una en una al cliente (ver método cargar preguntas)
         for pregunta in self.preguntas:
                 
             self.socket.send(str('P;' + (pregunta[1] + '\n' + pregunta[2] + '\n' + pregunta[3] + '\n' + pregunta[4]+ '\n' + pregunta[5])).encode())
+            
+            #recibimos la respuesta y la comprobamos si es correcta o no (ver método comprobarRespuesta)
             if comprobarRespuesta(pregunta[0], self.socket.recv(1024).decode()):
                 self.socket.send('Acierto'.encode())   
                 self.aciertos += 1
             else:
                 self.socket.send('Fallo'.encode())  
 
+        #una vez acabado el turno del jugador, se le envía su puntuación y lo mantenemos a la espera de que terminen los 4
         self.socket.send(str('FT;>>> Puntuación: ' + str(self.aciertos) + ' pts\nEsperando a que acaben el resto de jugadores...').encode())
 
+        #almacenamos su email y su numero de aciertos
         jugadores.__setitem__(self.email, self.aciertos)
 
         numJugadores = numJugadores - 1
 
-        cad = ""
+        cadena = ""
         jugadoresOrdenados = list()
         clasDic = {}
 
+        #cuando terminamos la partida, es decir, cuando el número de jugadores sea 0
         if numJugadores == 0:
-
+            
+            #usamos mutex para proteger el acceso al fichero
             mutex.acquire()
 
+            #creamos el fichero para almacenar la clasificación si no existe
             if (os.path.isfile('./clasificacion.txt') == False):
                 file = Path('./clasificacion.txt')
                 file.touch(exist_ok=True)
 
-            jugadoresOrdenados = sorted(jugadores.items(), key=operator.itemgetter(1))
+            #Ordenmos los jugadores por puntuación
+            jugadoresOrdenados = sorted(jugadores.items(), key=operator.itemgetter(1), reverse=True)
 
             clasificacion = open("./clasificacion.txt","r")
             
+            #almacenamos en un diccionario todos los registros de la clasificacion
             for linea in clasificacion:
                 datos = linea.split(";")
                 clasDic.__setitem__(datos[0], datos[1])
@@ -90,34 +106,43 @@ class Trivial(Thread):
             clasificacion.close()
             keys = clasDic.keys()
 
+            #recorremos la lista de jugadores ordenados
             for jugador in enumerate(jugadoresOrdenados):
-
+                
+                #si el jugadador aparece en la lista de claves se le suma los puntos actuales
+                # y se le asigna el nuevo valor en su posición, y si no, lo insertamos en el diccionario
                 if (jugador[1][0] in keys):
                     valor = int(clasDic[jugador[1][0]]) + int(jugadores[jugador[1][0]])
                     clasDic[jugador[1][0]] = valor
                 else:
                     clasDic.__setitem__(jugador[1][0], jugadores[jugador[1][0]])
 
-                cad += str(jugador[1][0]) + ' ' + str(jugador[1][1]) + "\n"
+                cadena += str(jugador[1][0]) + ' ' + str(jugador[1][1]) + "\n"
 
             general = open("./clasificacion.txt", "w")
             
+            #escribimos en el fichero de la clasificacion toda la clasificacion actualizada con
+            #las últimas puntuaciones
             for key, value in clasDic.items():
                 jugador = str(key) + ";" + str(value)
                 general.write(str(jugador) + "\n")
 
-            general.close()               
+            general.close() 
+
+            mostrarClasificacionGen()              
 
             mutex.release()
 
             sleep(1)
 
+        #esperamos a que no haya jugadores 
         while numJugadores != 0:
             pass
 
         sleep(tiempo)
         
-        self.socket.send(str('FP;\n>>> Fin de la partida <<<\n' + str(cad)).encode())
+        #enviamos a los jugadores, cómo han quedado en la partida
+        self.socket.send(str('FP;\n>>> Fin de la partida <<<\n' + str(cadena)).encode())
 
         turno.release()
 
@@ -205,8 +230,26 @@ def comprobarRespuesta(numPregunta, resp):
     fichero.close()
     return False
 
+def mostrarClasificacionGen():
 
-turno = Semaphore(4)
+    clasificacion = open("./clasificacion.txt", "r")
+    jugadores = {}
+
+    for l in clasificacion:
+        p = l.split(";")
+        jugadores.__setitem__(p[0], p[1])
+
+    clasificacion.close()
+
+    jugadoresOrdenados = sorted(jugadores.items(), key=operator.itemgetter(1), reverse=True)
+    pos = 0
+
+    for jugador in enumerate(jugadoresOrdenados):
+        pos += 1
+        print(str(pos) + ') ' + "{}".format(jugador[1][0]) + ' ' + "{}".format(jugadores[jugador[1][0]]))
+
+
+turno = Semaphore(2)
 mutex = Lock()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("",9004))
@@ -214,8 +257,8 @@ server.listen(1)
 
 jugadores = {}
 jugadoresOrdenados = {}
-cad = ''
 numJugadores = 0
+cadena = ''
 tiempo = 0
 listaPreguntas = cargarPreguntas()
 
